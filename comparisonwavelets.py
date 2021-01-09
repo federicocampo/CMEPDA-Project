619bf50f730826db4244ad9fe89e1268c83fd1d5
Federico Vincenzo Mastellone <mastellonefederico@gmail.com>
2021-01-09 11:48:54 +0100

    Added two level decomposition and reconstruction

--- comparisonwavelets.py
+++ comparisonwavelets.py
@@ -1,3 +1,51 @@
+def twolvldecomp(im_path, wavelet):
+    """ This function decompose the original image with a Discrete Wavelet Transformation
+        using the desired wavelet family up to the third level. It keeps all the details
+        coefficient and mask the resulted approximated image in order to enhance the
+        visibility of all the details.
+    """
+    myim = Image.open(im_path)
+
+    level = 2
+
+    cA, (cH2, cV2, cD2), (cH1, cV1, cD1) = pywt.wavedec2(myim, wavelet, level=level) # Here I get my approximated image and the relative coefficients
+
+    """ Now, I get the standard deviation for each matrix (image and coefficients).
+        The std will act as a treshold so that if abs(value) < 0. --> value = 0.
+                                             elif abs(value) > 0. --> value = value
+    """ 
+    mult_val = 1.
+
+    ncA = np.zeros_like(cA)
+
+    std10 = np.std(cH1)*mult_val
+    std11 = np.std(cV1)*mult_val
+    std12 = np.std(cD1)*mult_val
+
+    ncH1 = pywt.threshold(cH1, std10, mode = 'hard', substitute = 0.)
+    ncV1 = pywt.threshold(cV1, std11, mode = 'hard', substitute = 0.)
+    ncD1 = pywt.threshold(cD1, std12, mode = 'hard', substitute = 0.)
+
+    std20 = np.std(cH2)*mult_val
+    std21 = np.std(cV2)*mult_val
+    std22 = np.std(cD2)*mult_val
+
+    ncH2 = pywt.threshold(cH2, std20, mode = 'hard', substitute = 0.)
+    ncV2 = pywt.threshold(cV2, std21, mode = 'hard', substitute = 0.)
+    ncD2 = pywt.threshold(cD2, std22, mode = 'hard', substitute = 0.)
+
+    """ To let things be more readable I define new_coeff,
+        this is just so that waverec2 (the function needed to reconstruct
+        the image from a set of given coefficient) can do what it does.
+    """
+
+    new_coeff = ncA, (ncH2, ncV2, ncD2), (ncH1, ncV1, ncD1) 
+
+    mynewim = pywt.waverec2(new_coeff, wavelet)
+    mynewim = pywt.threshold(mynewim, 0., mode = 'greater', substitute = 0.)
+
+    return myim, mynewim
+
 def threelvldecomp(im_path, wavelet):
     """ This function decompose the original image with a Discrete Wavelet Transformation
         using the desired wavelet family up to the third level. It keeps all the details
@@ -6,37 +54,38 @@ def threelvldecomp(im_path, wavelet):
     """
     myim = Image.open(im_path)
 
-    wavelet = wavelet
     level = 3
     mode = 'periodization'
 
-    cA, (cH3, cV3, cD3), (cH2, cV2, cD2), (cH1, cV1, cD1) = pywt.wavedec2(myim, wavelet, mode=mode, level=level) # Here I get my approximated image and the relative coefficients
+    cA, (cH3, cV3, cD3), (cH2, cV2, cD2), (cH1, cV1, cD1) = pywt.wavedec2(myim, wavelet, level=level) # Here I get my approximated image and the relative coefficients
 
     """ Now, I get the standard deviation for each matrix (image and coefficients).
         The std will act as a treshold so that if abs(value) < 0. --> value = 0.
-                                            elif abs(value) > 0. --> value = value
-    """
+                                             elif abs(value) > 0. --> value = value
+    """ 
+    mult_val = 1.
+
     ncA = np.zeros_like(cA)
 
-    std10 = np.std(cH1)
-    std11 = np.std(cV1)
-    std12 = np.std(cD1)
+    std10 = np.std(cH1)*mult_val
+    std11 = np.std(cV1)*mult_val
+    std12 = np.std(cD1)*mult_val
 
     ncH1 = pywt.threshold(cH1, std10, mode = 'hard', substitute = 0.)
     ncV1 = pywt.threshold(cV1, std11, mode = 'hard', substitute = 0.)
     ncD1 = pywt.threshold(cD1, std12, mode = 'hard', substitute = 0.)
 
-    std20 = np.std(cH2)
-    std21 = np.std(cV2)
-    std22 = np.std(cD2)
+    std20 = np.std(cH2)*mult_val
+    std21 = np.std(cV2)*mult_val
+    std22 = np.std(cD2)*mult_val
 
     ncH2 = pywt.threshold(cH2, std20, mode = 'hard', substitute = 0.)
     ncV2 = pywt.threshold(cV2, std21, mode = 'hard', substitute = 0.)
     ncD2 = pywt.threshold(cD2, std22, mode = 'hard', substitute = 0.)
 
-    std30 = np.std(cH3)
-    std31 = np.std(cV3)
-    std32 = np.std(cD3)
+    std30 = np.std(cH3)*mult_val
+    std31 = np.std(cV3)*mult_val
+    std32 = np.std(cD3)*mult_val
 
     ncH3 = pywt.threshold(cH3, std30, mode = 'hard', substitute = 0.)
     ncV3 = pywt.threshold(cV3, std31, mode = 'hard', substitute = 0.)
@@ -49,7 +98,7 @@ def threelvldecomp(im_path, wavelet):
 
     new_coeff = ncA, (ncH3, ncV3, ncD3), (ncH2, ncV2, ncD2), (ncH1, ncV1, ncD1) 
 
-    mynewim = pywt.waverec2(new_coeff, wavelet, mode=mode)
+    mynewim = pywt.waverec2(new_coeff, wavelet)
     mynewim = pywt.threshold(mynewim, 0., mode = 'greater', substitute = 0.)
 
     return myim, mynewim
@@ -67,7 +116,7 @@ def fourlvldecomp(im_path, wavelet):
     level = 4
     mode = 'periodization'
 
-    cA, (cH4, cV4, cD4), (cH3, cV3, cD3), (cH2, cV2, cD2), (cH1, cV1, cD1) = pywt.wavedec2(myim, wavelet, mode=mode, level=level) # Here I get my approximated image and the relative coefficients
+    cA, (cH4, cV4, cD4), (cH3, cV3, cD3), (cH2, cV2, cD2), (cH1, cV1, cD1) = pywt.wavedec2(myim, wavelet, level=level) # Here I get my approximated image and the relative coefficients
 
     """ Now, I get the standard deviation for each matrix (image and coefficients).
         The std will act as a treshold so that if abs(value) < 0. --> value = 0.
@@ -114,7 +163,7 @@ def fourlvldecomp(im_path, wavelet):
 
     new_coeff = ncA, (ncH4, ncV4, ncD4), (ncH3, ncV3, ncD3), (ncH2, ncV2, ncD2), (ncH1, ncV1, ncD1) 
 
-    mynewim = pywt.waverec2(new_coeff, wavelet, mode=mode)
+    mynewim = pywt.waverec2(new_coeff, wavelet)
     mynewim = pywt.threshold(mynewim, 0., mode = 'greater', substitute = 0.)
 
     return myim, mynewim
@@ -234,8 +283,6 @@ def savecomparison(myim, mynewim, save_path, title_of_image, name_of_image):
     plt.savefig(final_path, bbox_inches='tight')
 
 
-
-
 from PIL import Image
 import pywt
 import matplotlib.pyplot as plt
@@ -247,7 +294,21 @@ import os
 """
 
 # BISOGNA MODIFICARE QUESTO PATH PER RICAVARE IL CORRETTO PATH DELL'IMMAGINE
-image_path = r"C:\Users\feder\Desktop\Computational Methods for Experimental Physics and Data Analysis\cmepda_medphys\L4_code\0039t1_2_1_1.pgm"
+image_path = r"C:\Users\feder\Desktop\Computational Methods for Experimental Physics and Data Analysis\IMAGES\Mammography_micro\Train\1\0088u1_2_1_1.pgm_3.pgm"
+
+for i, wavelet_type in enumerate(['haar', 'sym2', 'sym3', 'db2', 'db3']):
+    # Here I do the magic with pywavelets
+    myim, mynewim = twolvldecomp(image_path, wavelet_type)
+
+    # The image will be saved in a the 'Comparison' folder on the Desktop of the User
+    save_path = os.path.join(os.environ['USERPROFILE'], 'Desktop', 'Comparisons')
+
+    if not os.path.exists(save_path):
+        os.makedirs(save_path)
+
+    image_name = f"twolevelswith{wavelet_type}.png"
+    image_title = f"Altered with {wavelet_type}-2lvls"
+    savecomparison(myim, mynewim, save_path, image_title, image_name)
 
 for i, wavelet_type in enumerate(['haar', 'sym2', 'sym3', 'db2', 'db3']):
     # Here I do the magic with pywavelets
@@ -263,7 +324,7 @@ for i, wavelet_type in enumerate(['haar', 'sym2', 'sym3', 'db2', 'db3']):
     image_title = f"Altered with {wavelet_type}-3lvls"
     savecomparison(myim, mynewim, save_path, image_title, image_name)
 
-for i, wavelet_type in enumerate(['haar', 'sym2', 'sym3', 'db2', 'db3']):
+"""for i, wavelet_type in enumerate(['haar', 'sym2', 'sym3', 'db2', 'db3']):
     # Here I do the magic with pywavelets
     myim, mynewim = fourlvldecomp(image_path, wavelet_type)
 
@@ -289,4 +350,4 @@ for i, wavelet_type in enumerate(['haar', 'sym2', 'sym3', 'db2', 'db3']):
 
     image_name = f"fivelevelswith{wavelet_type}.png"
     image_title = f"Altered with {wavelet_type}-5lvls"
-    savecomparison(myim, mynewim, save_path, image_title, image_name)
+    savecomparison(myim, mynewim, save_path, image_title, image_name)"""
