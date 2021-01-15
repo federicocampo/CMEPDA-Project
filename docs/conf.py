8c25438e2e48c3663dbfa44b7b33cf2dd9ed4a72
Federico Vincenzo Mastellone <mastellonefederico@gmail.com>
2021-01-15 18:27:27 +0100

    minor

--- docs/conf.py
+++ docs/conf.py
@@ -62,4 +62,4 @@ html_theme = 'sphinx_rtd_theme'
 # so a file named "default.css" will overwrite the builtin "default.css".
 html_static_path = []
 
-# autodoc_mock_imports = ['skimage']
+autodoc_mock_imports = ['skimage','matplotlib']
